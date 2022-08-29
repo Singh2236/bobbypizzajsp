b1db61da2fb9a23de365872a2d3d8a28c92466ed
singh2236 <nav.kang4@gmail.com>
2022-08-29 09:20:50 +0200

    init databases

---
+++ src/main/java/com/navpreet/database/database.java
@@ -0,0 +1,19 @@
+package com.navpreet.database;
+
+import java.sql.*;
+
+public class database {
+    public static void main(String[] args) throws SQLException {
+        Connection connection = DriverManager
+                .getConnection("jdbc:mysql://localhost:3306/pizzashop","pizzashop","pizzashop");
+        Statement statement = connection.createStatement();
+
+        ResultSet resultSet = statement.executeQuery("SELECT * From Menu");
+
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("name"));
+        }
+
+        System.out.println("DONE");
+    }
+}
